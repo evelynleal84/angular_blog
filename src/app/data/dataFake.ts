dbb7a21f8658dbc7704c03da6b0bb593e07a979c
evelynleal84 <167266225+evelynleal84@users.noreply.github.com>
2025-04-13 15:03:17 -0300

    alteração

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Top 4: Melhores bandas dos tempos modernos",
+    "description": "Top 1: Foo Fighters (EUA).Lendas vivas do rock alternativo, continuam com shows lotados e novo álbum elogiado.",
+    "photoCover":"https://cdn.wegow.com/media/artists/foo-fighters/foo-fighters-1581339172.4464262.2560x1440.jpg"
+  },
+  {
+    "id":"2",
+    "title": "TOP 2: Bring Me The Horizon (Reino Unido)",
+    "description": "Mistura metalcore com pop eletrônico. Sempre reinventando o som e atraindo público jovem.",
+    "photoCover":"https://www.metalmusicarchives.com/images/artists/bring-me-the-horizon-20170221093942.jpg"
+  },
+  {
+    "id":"3",
+    "title": "TOP 3: Arctic Monkeys (Reino Unido)",
+    "description": "Indie rock refinado com presença global. Shows sempre concorridos.",
+    "photoCover":"https://ligadoamusica.com.br/wp-content/uploads/2022/10/18_OUTUBRO_Arctic-Monkeys-pc-Zackery-Michael-436763_L2-B_08-300-dpi.jpg"
+  },
+
+  {
+    "id":"4",
+    "title": "TOP 4: Ghost (Suécia)" ,
+    "description": "Rock teatral com estética marcante. Mistura heavy metal com pop e ópera rock.",
+    "photoCover":"https://www.diariodeunrockero.es/wp-content/uploads/2023/04/ghost-1637925769-1637925769.390646.-1x1780-1.jpg"
+  },
+]
